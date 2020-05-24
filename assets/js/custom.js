1bffcc5c4c30c0f2e5adc99d2c8dd87759029381
CarlosFriasMartinez <carlospronet24@gmail.com>
2020-05-24 12:57:55 -0500

    first commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,2 @@
 < script data - ad - client = "ca-pub-8410634237915855"
-async src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" >
-    <
-    /script>
+async src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" > < /script>
